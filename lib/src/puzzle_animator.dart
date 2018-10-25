e995a7731574b1bb7b1a27fa99470861d40f794a
Kevin Moore <kevmoo@google.com>
2018-10-25 13:38:53 -0700

    use Body in PuzzleAnimator

--- lib/src/puzzle_animator.dart
+++ lib/src/puzzle_animator.dart
@@ -1,18 +1,20 @@
 import 'dart:math';
 
+import 'body.dart';
 import 'puzzle.dart';
 
 class PuzzleAnimator {
   final Puzzle puzzle;
-  final List<Point<double>> _locations;
+  final List<Body> _locations;
 
   bool _stable;
 
-  Point<double> location(int index) => _locations[index];
+  Point<double> location(int index) => _locations[index].location;
 
   PuzzleAnimator(this.puzzle)
       : _locations = List.generate(puzzle.length, (i) {
-          return Point((puzzle.width - 1.0) / 2, (puzzle.height - 1.0) / 2);
+          return Body.raw(
+              (puzzle.width - 1.0) / 2, (puzzle.height - 1.0) / 2, 0, 0);
         });
 
   bool get stable => _stable;
@@ -25,24 +27,28 @@ class PuzzleAnimator {
   void update(Duration timeDelta) {
     assert(!timeDelta.isNegative);
     assert(timeDelta != Duration.zero);
-    final maxDistance = timeDelta.inMilliseconds / 200.0;
+
+    var animationSeconds = timeDelta.inMilliseconds / 60.0;
+    if (animationSeconds == 0) {
+      animationSeconds = 0.1;
+    }
+    assert(animationSeconds > 0);
+
+    var maxVelocity = 0.0;
 
     _stable = true;
     for (var i = 0; i < puzzle.length; i++) {
       final target = _target(i);
-      final current = _locations[i];
+      final body = _locations[i];
 
-      if (target != current) {
-        _stable = false;
+      final delta = (target - body.location);
+      final force = delta;
 
-        var locationDelta = target - current;
+      _stable = !body.animate(animationSeconds,
+              force: force, drag: 0.8, maxVelocity: 1.0) &&
+          _stable;
 
-        if (locationDelta.magnitude > maxDistance) {
-          locationDelta *= (maxDistance / locationDelta.magnitude);
-        }
-
-        _locations[i] = current + locationDelta;
-      }
+      maxVelocity = max(maxVelocity, body.velocity.magnitude);
     }
   }
 }
